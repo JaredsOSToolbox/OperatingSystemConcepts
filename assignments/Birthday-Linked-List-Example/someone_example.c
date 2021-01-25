b22a0a8593a4e933b264990515871f3b76c12038
JaredDyreson <jared.dyreson@gmail.com>
2021-01-24 18:55:16 -0800

    things seem to work now

--- assignments/Birthday-Linked-List-Example/someone_example.c
+++ assignments/Birthday-Linked-List-Example/someone_example.c
@@ -43,55 +43,34 @@ inline birthday* birthday_constructor(int d){
     return person;
 }
 
-/*void birthday_container_append(struct birthday* birthday_object){*/
-    /*if(head->tail == NULL){*/
-        /*list_add_tail(&birthday_object->list, &head);*/
-    /*} else {*/
-        /*struct birthday* prev = */
-    /*}*/
-/*}*/
-
 static LIST_HEAD(head);
 
 int linkedlist_init(void) {
     // insert the elements into the list
     // we're doing it in reverse order now so when it is retrieved it is in the correct order
 
-    for(int i = 0; i < 5; ++i){
-        birthday* bday = birthday_constructor(i);
-        if(i == 0) {
-            list_add_tail(&head, &bday->list);
-        } else {
-            struct birthday *prev = list_last_entry(&head, struct birthday, list);
-            list_add_tail(&prev->list, &bday->list);
-        }
-    }
-
-    /*birthday* e0 = birthday_constructor(0);*/
-    /*birthday* e1 = birthday_constructor(1);*/
-    /*birthday* e2 = birthday_constructor(2);*/
-    /*birthday* e3 = birthday_constructor(3);*/
-    /*birthday* e4 = birthday_constructor(4);*/
-    
-    birthday* pos;
-    birthday* n;
-    
     /*
       head->e0->e1->e2->e3->e4->(head)
     */
 
-    /*list_add_tail(&e4->list, &head);*/
-    /*list_add_tail(&e3->list, &e4->list);*/
-    /*list_add_tail(&e2->list, &e3->list);*/
-    /*list_add_tail(&e1->list, &e2->list);*/
-    /*list_add_tail(&e0->list, &e1->list);*/
-    
-    // think of this as a for loop, defined in /lib/modules/4.15.0-132-generic/build/include/linux/list.h:472
-
-    list_for_each_entry_safe(pos, n, &head, list) {
-        struct birthday *object =  pos;
-        printk(KERN_INFO "here baby cakes\n");
+    int i;
+    for(i = 5; i >= 0; --i){
+        birthday* bday = birthday_constructor(i);
+        if(i == 0) {
+            /*list_add_tail(&head, &bday->list);*/
+            list_add_tail(&bday->list, &head);
+        } else {
+            birthday *prev = list_first_entry(&head, struct birthday, list);
+            /*list_add_tail(&prev->list, &bday->list);*/
+            list_add_tail(&bday->list, &prev->list);
+        }
+        printk(KERN_INFO "[INFO] Inserting value of %d into the birthday list\n", i);
     }
+
+    
+    birthday* pos;
+    birthday* n;
+    
 
     return 0;
 }
@@ -99,7 +78,14 @@ int linkedlist_init(void) {
 void linkedlist_exit(void){  
     birthday* pos;
     birthday* n;
-    list_for_each_entry_safe(pos, n, &head, list);
+
+    // think of this as a for loop, defined in /lib/modules/4.15.0-132-generic/build/include/linux/list.h:472
+
+    list_for_each_entry_safe(pos, n, &head, list) {
+        printk(KERN_INFO "[INFO] Removing value of %d from the list\n", pos->day);
+        list_del(&(pos->list));
+        kfree(pos);
+    }
     printk(KERN_INFO "Removing Module Done\n");
 }
 
