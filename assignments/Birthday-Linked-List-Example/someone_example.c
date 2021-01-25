5a0bacb291bff9ead6593ced3ae3ee859f7b6972
JaredDyreson <jared.dyreson@gmail.com>
2021-01-24 18:17:52 -0800

    trying this new insertion method, going to make a save state

--- assignments/Birthday-Linked-List-Example/someone_example.c
+++ assignments/Birthday-Linked-List-Example/someone_example.c
@@ -5,66 +5,100 @@
 #include <linux/slab.h>
 
 #define LICENSE "GPL"
-#define DESCRIPTION "Ch2_LinkedList"
-#define AUTHOR "jz33"
+#define DESCRIPTION "McCarthy Assignment 0"
+#define AUTHOR "Jared Dyreson"
+/*
+ * Adapated from these two sources:
+ *
+ * https://stackoverflow.com/questions/16230524/explain-list-for-each-entry-and-list-for-each-entry-safe
+ * https://github.com/jz33/C-OS-Solution-Manual/blob/master/2_1.c
+ *
+ * I took one look at Kernel dev and said, "that's a no from me Chief"
+*/
 
 MODULE_LICENSE(LICENSE);
 MODULE_DESCRIPTION(DESCRIPTION);
 MODULE_AUTHOR(AUTHOR);
 
-/*
-Operating System Concepts Edition 9th Project 2.1
-Test on Debian 7
-Auther: junzhengrice@gmail.com
-*/
-
-//Simplified birthday struct
 struct birthday{
     int day;
     struct list_head list;
 };
-typedef struct birthday bday;
 
-//Initialize bday list
-inline bday* bday_init(int d){
-    bday* person = kmalloc(sizeof(bday), GFP_KERNEL);
+typedef struct birthday birthday;
+// instead of saying struct birthday birthday, just struct birthday
+
+struct birthday_container {
+    struct list_head list;
+    struct birthday birthday_;
+};
+
+// Initialize birthday list
+// Move to header file
+
+inline birthday* birthday_constructor(int d){
+    birthday* person = kmalloc(sizeof(birthday), GFP_KERNEL);
     person->day = d;
     INIT_LIST_HEAD(&person->list);
     return person;
 }
 
-//Tranverse macro
+/*void birthday_container_append(struct birthday* birthday_object){*/
+    /*if(head->tail == NULL){*/
+        /*list_add_tail(&birthday_object->list, &head);*/
+    /*} else {*/
+        /*struct birthday* prev = */
+    /*}*/
+/*}*/
 
 static LIST_HEAD(head);
 
-int linkedlist_init(void){
+int linkedlist_init(void) {
+    // insert the elements into the list
+    // we're doing it in reverse order now so when it is retrieved it is in the correct order
+
+    for(int i = 0; i < 5; ++i){
+        birthday* bday = birthday_constructor(i);
+        if(i == 0) {
+            list_add_tail(&head, &bday->list);
+        } else {
+            struct birthday *prev = list_last_entry(&head, struct birthday, list);
+            list_add_tail(&prev->list, &bday->list);
+        }
+    }
+
+    /*birthday* e0 = birthday_constructor(0);*/
+    /*birthday* e1 = birthday_constructor(1);*/
+    /*birthday* e2 = birthday_constructor(2);*/
+    /*birthday* e3 = birthday_constructor(3);*/
+    /*birthday* e4 = birthday_constructor(4);*/
     
-    bday* e0 = bday_init(0);
-    bday* e1 = bday_init(1);
-    bday* e2 = bday_init(2);
-    bday* e3 = bday_init(3);
-    bday* e4 = bday_init(4);
-    
-    bday* pos;
-    bday* n;
+    birthday* pos;
+    birthday* n;
     
     /*
       head->e0->e1->e2->e3->e4->(head)
     */
-    list_add_tail(&e4->list, &head);
-    list_add_tail(&e3->list, &e4->list);
-    list_add_tail(&e2->list, &e3->list);
-    list_add_tail(&e1->list, &e2->list);
-    list_add_tail(&e0->list, &e1->list);
-        
-    list_for_each_entry_safe(pos, n, &head, list);
+
+    /*list_add_tail(&e4->list, &head);*/
+    /*list_add_tail(&e3->list, &e4->list);*/
+    /*list_add_tail(&e2->list, &e3->list);*/
+    /*list_add_tail(&e1->list, &e2->list);*/
+    /*list_add_tail(&e0->list, &e1->list);*/
     
+    // think of this as a for loop, defined in /lib/modules/4.15.0-132-generic/build/include/linux/list.h:472
+
+    list_for_each_entry_safe(pos, n, &head, list) {
+        struct birthday *object =  pos;
+        printk(KERN_INFO "here baby cakes\n");
+    }
+
     return 0;
 }
 
 void linkedlist_exit(void){  
-    bday* pos;
-    bday* n;
+    birthday* pos;
+    birthday* n;
     list_for_each_entry_safe(pos, n, &head, list);
     printk(KERN_INFO "Removing Module Done\n");
 }
